25e2b170459f963ff336ac244ae9fa37c16a0eea
LisaWehbrink <lwehbrink@t-online.de>
2018-10-31 14:23:13 +0100

    Input logic

--- technical_test/src/technical_test/Main.java
+++ technical_test/src/technical_test/Main.java
@@ -1,9 +1,17 @@
 package technical_test;
 
+
+
 public class Main {
 
 	
 	public static void main(String[] args) {
+		Request request = new Request("Dave");
+		RequestController controller = new RequestController();
+		
+		controller.fillRequest(request);
+		
+		System.out.println(request.getPassengers());
 		
 	}
 }
